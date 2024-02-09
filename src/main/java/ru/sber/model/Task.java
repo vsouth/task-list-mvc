b0c58c86efa31057b053671d4dfbcf94b4bd181d
g943_shishkin.a.i <astopikov@gmail.com>
2024-02-10 01:14:35 +0300

    cleanup

--- src/main/java/ru/sber/model/Task.java
+++ src/main/java/ru/sber/model/Task.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
@@ -17,8 +14,8 @@ public class Task {
     @NonNull
     private int id;
     @NonNull
-    @NotEmpty(message = "Название не должно быть пустым!")
-    @Size(min = 5, max = 32, message = "Название должно быть от 5 до 32 символов!")
+//    @NotEmpty(message = "Название не должно быть пустым!")
+//    @Size(min = 5, max = 32, message = "Название должно быть от 5 до 32 символов!")
     private String title;
     private String description;
     @NonNull
@@ -26,14 +23,10 @@ public class Task {
     @NonNull
     private LocalDateTime createdAt;
     private LocalDateTime completedAt;
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    //    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadlineAt;
     @NonNull
     private boolean isCompleted;
     @NonNull
     private User creator;
-
-    public Task(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
