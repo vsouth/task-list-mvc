cb8babf394cba3549583b2fb920403a8d150aeae
g943_shishkin.a.i <astopikov@gmail.com>
2024-02-10 01:00:41 +0300

    cleanup

--- src/main/java/ru/sber/controller/TaskController.java
+++ src/main/java/ru/sber/controller/TaskController.java
@@ -2,15 +2,8 @@ package ru.sber.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.sber.model.Task;
 import ru.sber.repository.TaskRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,22 +11,4 @@ import java.util.Optional;
 public class TaskController {
     private final TaskRepository taskRepository;
 
-    @GetMapping
-    public String show(Model model) {
-        List<Task> tasks = taskRepository.findAll();
-//        log.info("#Выводим список задач");
-        model.addAttribute("tasks", tasks);
-        return "list";
-    }
-
-    @GetMapping("/{id}")
-    public String showById(@PathVariable("id") int id, Model model) {
-        Optional<Task> mbTask = taskRepository.findById(id);
-//        log.info("Показавыем задачу с id = " + id);
-//        log.info("Пытаемся вытащить атрибут" + model.getAttribute("tasks"));
-        mbTask.ifPresent(task -> {
-            model.addAttribute("task", task);
-        });
-        return "show";
-    }
 }
