c828fe40e3bb889788d53ecf90db4388d9924e48
vsouth <neppchela@gmail.com>
2024-02-23 21:20:29 +0300

    added user controller and sessions

---
+++ src/main/java/ru/sber/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.sber.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.sber.model.User;
+import ru.sber.repository.UserRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+    private final UserRepository userRepository;
+
+    @PostMapping("/login")
+    public String userAuth(@ModelAttribute @Valid User user, HttpSession session, HttpServletRequest request) {
+        log.info("#auth user: " + user);
+        Optional<User> existingUser = userRepository.getUserByLogin(user.getLogin());
+        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+            session.setAttribute("user-id", existingUser.get().getId());
+            return "redirect:/tasks";
+        }
+        return "redirect:/index";
+    }
+
+    @PostMapping("/register")
+    public String create(@ModelAttribute @Valid User user, HttpSession session, HttpServletRequest request) {
+        log.info("#register user: " + user);
+        userRepository.createUser(user);
+        session.setAttribute("user-id", user.getId());
+        return "redirect:/tasks";
+    }
+}
--- src/main/java/ru/sber/model/User.java
+++ src/main/java/ru/sber/model/User.java
@@ -1,11 +1,18 @@
 package ru.sber.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
 
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Scope("session")
 public class User {
-    private final int id;
+    @NonNull
+    private int id;
     @NonNull
     private String login;
     @NonNull
