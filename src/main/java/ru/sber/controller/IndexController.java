06ca1d4215507590275e9989859c94a5ab8f65a1
vsouth <neppchela@gmail.com>
2024-02-23 21:17:37 +0300

    added login and register at index page

--- src/main/java/ru/sber/controller/IndexController.java
+++ src/main/java/ru/sber/controller/IndexController.java
@@ -2,12 +2,18 @@ package ru.sber.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import ru.sber.model.Priority;
+import ru.sber.model.Task;
+import ru.sber.model.User;
 
 @Controller
 public class IndexController {
     @GetMapping("/index")
-    public String index() {
-        System.out.println("hello!");
-        return "index";
+    public ModelAndView showIndex(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        User user = new User();
+        modelAndView.addObject(user);
+        return modelAndView;
     }
 }
